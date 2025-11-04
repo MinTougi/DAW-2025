44fa28df089f9d38a12a15c8d6eff78b3b7af504
Rafaela <rafaela.gaievski30@gmail.com>
2025-11-04 08:52:54 -0300

    30/10

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
 import { Header } from "./components/layout/header"
 import { Footer } from "./components/layout/footer"
-=======
->>>>>>> dba334fa0c616468fecf41d11135b14a2b6ed6e6
+import { Navbar } from "./components/layout/navbar"
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <Header/>
+      <Navbar />
       <Footer/>
-=======
-    
->>>>>>> dba334fa0c616468fecf41d11135b14a2b6ed6e6
     </>
   )
 }
 
-<<<<<<< HEAD
 export default App
-=======
-export default App
->>>>>>> dba334fa0c616468fecf41d11135b14a2b6ed6e6
